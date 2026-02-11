c9c5831d46b0b3a0fb6756fff051e649552b6c43
Kanishkasrivadivel <153812835+Kanishkasrivadivel@users.noreply.github.com>
2026-02-11 16:52:10 +0530

    Add files via upload

---
+++ init_db.py
@@ -0,0 +1,11 @@
+from .database import engine
+from .models import Base
+
+
+def init():
+    Base.metadata.create_all(bind=engine)
+
+
+if __name__ == "__main__":
+    init()
+    print("PostgreSQL tables created ✅")
